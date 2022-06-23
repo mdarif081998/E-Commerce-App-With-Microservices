41035826c9781a5a61f55e0c8cd509f336ac6d14
mdarif081998 <95627770+mdarif081998@users.noreply.github.com>
2022-06-23 13:25:36 +0530

    Add files via upload
    slueth and zipkin addition

--- Supplier-Service/src/main/java/com/md/arif/controller/SupplierController.java
+++ Supplier-Service/src/main/java/com/md/arif/controller/SupplierController.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,10 +18,14 @@ import com.md.arif.model.Supplier;
 import com.md.arif.service.SupplierService;
 
 @RestController
+@RefreshScope
 public class SupplierController {
 	
 	@Autowired
 	private SupplierService supservice;
+	
+	@Value("${supplier.test}")
+	private String test;
 	
 	
 //	private Supplier supplier;
@@ -50,6 +56,11 @@ public class SupplierController {
 	public void deleteSupplierById(@PathVariable("supplierId") int supplierId) {
 		 supservice.deleteSupplier(supplierId);
 		
+	}
+	
+	@GetMapping("/test")
+	public String test() {
+		return test;
 	}
 	
 
